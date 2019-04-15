e61483cff6bbf28e22bc037f0fb81cc7ec399e09
etienne perreau <etienne.perreau@edu.ece.fr>
2019-04-15 15:33:01 +0200

    afficher 1st valeur fichier texte

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
-#include <iostream>
 
+#include <iostream>
+#include <string>
+#include <fstream>
 using namespace std;
 
-int main()
-{
-    cout << "Hello world ssuper on adore i feel lonely sku!" << endl;
-    cout << "je crois que j'ai compris comment ca marche " << endl;
 
-    return 0;
+
+
+
+
+int main()
+
+{
+        ifstream fichier("broadway.txt", ios::in);
+
+        if(fichier)
+        {
+                int entier1;
+
+
+                fichier >> entier1 ;  /*on lit jusqu'à l'espace et on stocke ce qui est lu dans la variable indiquée */
+                std::cout << entier1 << endl;
+                fichier.close();
+
+        }
+        else
+                cerr << "Impossible d'ouvrir le fichier !" << endl;
+
+        return 0;
 }
+
+
